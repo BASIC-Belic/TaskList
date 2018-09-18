0dcca06a4db3d01adf498a759c2ffe076bdfc58a
Chantelle Belic <chantelle.belic@gmail.com>
2018-09-18 15:05:11 -0700

    changed show

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,16 +1,15 @@
 class TasksController < ApplicationController
 
 
-  TASKS = ["laundry", "eating", "ruby homework"]
-
-
   def index
     @tasks = Task.all
   end
 
   def show
+    @tasks ||= Task.all
     index = params[:id].to_i
-    @task = TASKS[index - 1]
+
+    @task = @tasks[index - 1][:action]
     if @task.nil?
       head :not_found
     end
