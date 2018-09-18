4903c7bd4a58268f94c2b234e183c334c973a619
Chantelle Belic <chantelle.belic@gmail.com>
2018-09-18 15:02:49 -0700

    pdate the controller's index action to retrieve and show all Tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,19 @@
 class TasksController < ApplicationController
+
+
+  TASKS = ["laundry", "eating", "ruby homework"]
+
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    index = params[:id].to_i
+    @task = TASKS[index - 1]
+    if @task.nil?
+      head :not_found
+    end
+  end
+
 end
