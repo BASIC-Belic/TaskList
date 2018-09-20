3f0c55f3908ff63580c760d364d40fca634d23ad
Chantelle Belic <chantelle.belic@gmail.com>
2018-09-20 10:04:00 -0700

    new/create controller actions and route instructions addd, ned to connect to new view form

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,14 +7,39 @@ class TasksController < ApplicationController
 
   def show
 
-    index = (params[:id].to_i)
+    # index = params[:id].to_i
 
-    @task = Task.find_by(id: index)
+    @task = Task.find_by(id: params[:id].to_i)
 
     if @task.nil?
       render :invalid_task_page, status: :not_found
       # head :not_found
     end
   end
+
+  def new
+    @task = Task.new
+  end
+
+  <p>Action: <%= @task.action%></p>
+  <p>Description: <%= @task.description%></p>
+  <p>Completion Date: <%= @task.completion_date%></p>
+
+  def create
+    task = Task.new(action: params[:task][:action],
+      description: params[:task][:description],
+      completion_date: params[:task][:completion_date])
+
+      if task.save
+        redirect_to tasks-path
+        #otherwise redirect to the newly created book page, two options:
+        # redirect_to task_path(book.id)
+        # redirect_to task_path(book)
+
+      else
+        
+        render new_task_path
+      end
+    end
 
 end
